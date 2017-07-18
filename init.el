60f4204f90de3ae0134ec8f84d3ab370f9fff619
Sijia Wang <sijiaoh@outlook.com>
2017-07-18 12:58:46 +0900

    Add web-mode

--- init.el
+++ init.el
@@ -73,6 +73,18 @@ locate PACKAGE."
     projectile-rails-spring-command "bin/spring"))
 
 
+;;; HTML
+(when (maybe-require-package 'web-mode)
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+
+
 ;;; Final
 (when (file-exists-p custom-file)
   (load custom-file))
