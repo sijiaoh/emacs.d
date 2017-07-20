b748476c2330ada43d51f3c3beff47054b285391
Sijia Wang <sijiaoh@outlook.com>
2017-07-20 14:52:38 +0900

    Enable auto quoting after = inside tag

--- init.el
+++ init.el
@@ -202,7 +202,9 @@ locate PACKAGE."
 
   ;; Auto close tag
   (setq web-mode-enable-auto-closing t)
-  (setq web-mode-enable-auto-pairing t))
+  (setq web-mode-enable-auto-pairing t)
+  ;; Auto quoting after = inside tag
+  (setq web-mode-enable-auto-quoting t))
 
 
 ;;; TypeScript
