48f6365dc6910d61de42c339acec23aabaf691c0
Sijia Wang <sijiaoh@outlook.com>
2017-07-20 14:44:57 +0900

    Enable auto close tag in web-mode

--- init.el
+++ init.el
@@ -198,7 +198,11 @@ locate PACKAGE."
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+  ;; Auto close tag
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-pairing t))
 
 
 ;;; TypeScript
