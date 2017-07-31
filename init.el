23416f4352c234e69ce2a3574c186363ac6b5f66
Sijia Wang <sijiaoh@outlook.com>
2017-07-31 17:55:26 +0900

    Add ddskk

--- init.el
+++ init.el
@@ -236,6 +236,25 @@ locate PACKAGE."
 (require-package 'groovy-mode)
 
 
+;;; ddskk
+(when (maybe-require-package 'ddskk)
+  (setq skk-server-prog "google-ime-skk")      ; google-ime-skk の場所
+  (setq skk-server-inhibit-startup-server nil) ; 辞書サーバが起動していなかったときに Emacs からプロセスを立ち上げる
+  (setq skk-server-host "localhost")           ; サーバー機能を利用
+  (setq skk-server-portnum 55100)              ; ポートは google-ime-skk
+  (setq skk-share-private-jisyo t)             ; 複数 skk 辞書を共有
+
+  ;; C-x C-j で skk-mode 起動
+  (global-set-key (kbd "C-x C-j") 'skk-mode)
+  ;; ミニバッファでは C-j を改行にしない
+  (define-key minibuffer-local-map (kbd "C-j") 'skk-kakutei)
+  ;; ";" を sticky キーに設定
+  (setq skk-sticky-key ";")
+  ;; isearch-mode でも使用可能にする
+  (add-hook 'isearch-mode-hook 'skk-isearch-mode-setup)
+  (add-hook 'isearch-mode-end-hook 'skk-isearch-mode-cleanup))
+
+
 ;;; eww
 ;; 背景・文字色を無効化する
 (defvar eww-disable-colorize t)
